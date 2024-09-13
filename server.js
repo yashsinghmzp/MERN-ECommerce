bfd84783ea522c62a8ce2e727e18198699ebbccd
yash kumar singh <yashsinghkhanpur7@gmail.com>
2024-09-14 00:26:29 +0530

    ready

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get("*", function (req, res) {
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
